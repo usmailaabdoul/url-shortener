e4bed6eb65a48d829b7b797f88a18405c63a9c25
usmailaabdoul <ismaelabdul77@gmail.com>
2021-12-16 22:33:07 +0100

    Adding icon components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 export {default as PrimmaryButton} from './button/primaryButton';
 export {default as SecondaryButton} from './button/secondaryButtton';
 export {default as FlatButton} from './button/flatButton';
-export {default as IconButton} from './button/iconButtton';
+export {default as IconButtons} from './button/iconButttons';
+export {default as ShortenUrlCard} from './card/shortenUrlCard';
+export {default as UrlCard} from './card/urlCard';
+export {default as BodyCard} from './card/bodyCard';
