bbde172e16ef0c0e7dc98ac4ebb9e1e026b894b0
usmailaabdoul <ismaelabdul77@gmail.com>
2021-12-16 22:34:02 +0100

    Adding url card

--- src/components/button/secondaryButtton.tsx
+++ src/components/button/secondaryButtton.tsx
@@ -6,7 +6,7 @@ const StyledButton = styled.button<{
   copied?: boolean;
 }>`
   border: none;
-  width: ${({ size }) => size === 's' ? '110px' : '150px'};
+  width: ${({ size }) => size === 's' ? '110px' : '170px'};
   height: ${({ size }) => size === 's' ? '40px' : '45px'};
   border-radius: ${({ size }) => size === 's' ? '4px' : '8px'};
   background-color: ${({ theme, copied }) => copied ? theme.colors.primary_dark_violet : theme.colors.primary_cyan};
@@ -16,9 +16,8 @@ const StyledButton = styled.button<{
     background-color: ${({ theme, copied }) => copied ? theme.colors.primary_dark_violet : theme.colors.primary_cyan_light};
   }
 
-  a {
+  .label {
     color: ${({ theme }) => theme.colors.white};
-    text-decoration: none;
     font-weight: 700;
   }
 `;
@@ -26,13 +25,12 @@ const StyledButton = styled.button<{
 const SecondaryButton: FC<{
   label: string,
   onClick: () => void,
-  link: string,
   size?: 'm' | 's',
   copied?: boolean,
-}> = ({label, onClick, link, size = 's', copied = false}) => {
+}> = ({label, onClick, size = 'm', copied = false}) => {
   return (
     <StyledButton onClick={onClick} size={size} copied={copied}>
-      <a href={link}>{label}</a>
+      <span className="label">{label}</span>
     </StyledButton>
   )
 }
--- src/components/card/urlCard.tsx
+++ src/components/card/urlCard.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { useState } from 'react';
+import styled from 'styled-components'
+import SecondaryButton from '../button/secondaryButtton';
+
+const StyleContainer = styled.div`
+  background: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.gray};
+  width: 100%;
+  height: 65px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 0px;
+  padding: 0 20px;
+
+  .main-url {
+    color: ${({ theme }) => theme.colors.dark_violet};
+    font-weight: 500;
+  };
+
+  .secondary-url {
+    color: ${({ theme }) => theme.colors.primary_cyan};
+    font-weight: 500;
+    margin-right: 20px;
+  }
+`;
 
 const UrlCard = () => {
+  const [copied, setCopied] = useState(false);
+
   return (
-    <div>
-      UrlCard
-    </div>
+    <StyleContainer>
+      <span className="main-url">http://localhost:3000/</span>
+      <div>
+        <span className="secondary-url">http://local</span>
+        <SecondaryButton label={copied? "Copied!": "Copy"} onClick={() => setCopied(true)} size='s' copied={copied} />
+      </div>
+    </StyleContainer>
   )
 }
 
-export default UrlCard
+export default UrlCard;
