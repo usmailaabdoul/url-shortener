3a33256248ffe374cffe80e155a33caeea9ac47f
usmailaabdoul <ismaelabdul77@gmail.com>
2021-12-22 20:33:50 +0100

    fixing ios mobile responsiveness issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import './App.css'
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/globalStyles';
 import {Home} from './pages';
+import {isIOS} from 'react-device-detect';
+import {useIsMobileDevice} from './utils/deviceWidth';
 
 function App() {
+  const isMobile = useIsMobileDevice();
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Home />
-      </ThemeProvider>
       <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <div className={isIOS || isMobile ? 'mobile-wrapper' : ''}>
+          <Home />
+        </div>
+      </ThemeProvider>
     </>
   );
 }
